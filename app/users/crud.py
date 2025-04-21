9253fea89c1cdcfb251d2a46a28725a3e1d0c147
romaperec <cpser621@gmail.com>
2025-04-21 09:56:47 +0300

    ruff fix

--- app/users/crud.py
+++ app/users/crud.py
@@ -7,6 +7,7 @@ from app.users.models import UserModel
 from app.users.service import hash_password
 from app.users.service import verify_password
 
+
 async def create(user_name, user_email, user_password, db: AsyncSession):
     check_email = await db.execute(
         select(UserModel).where(UserModel.email == user_email)
@@ -27,15 +28,13 @@ async def create(user_name, user_email, user_password, db: AsyncSession):
 
 
 async def login(user_email, user_password, db: AsyncSession):
-    user_db = await db.execute(
-        select(UserModel).where(UserModel.email == user_email)
-    )
+    user_db = await db.execute(select(UserModel).where(UserModel.email == user_email))
     user_db = user_db.scalar_one_or_none()
 
     if not user_db:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Account not found"
         )
-    
+
     if verify_password(user_password, user_db.password):
         return user_db
